aeaedee7120bf97c394a05726efa4f7d8a7cd7a3
adithya-adari <adariadithya816@gmail.com>
2026-07-13 14:50:42 +0530

    Modify DB connection and add DNS servers
    Updated MongoDB connection string to use 'Contactmanager' database and added DNS server configuration.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const dns = required("dns")
+dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://adariadithya816_db_user:adithya@9059@cluster0.qfimjmp.mongodb.net/aditya");
+        await mongoose.connect("mongodb+srv://adariadithya816_db_user:adithya@9059@cluster0.qfimjmp.mongodb.net/Contactmanager");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
