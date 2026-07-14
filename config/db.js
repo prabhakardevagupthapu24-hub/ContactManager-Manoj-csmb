099532e8e317d95f118d1f5d4f67f12cab5c3819
prabhakardevagupthapu24-hub <prabhakar.devagupthapu24@sasi.ac.in>
2026-07-14 10:53:42 +0530

    Update MongoDB connection string with database name

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const dns = require("dns")
 dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://prabhakardevagupthapu24_db_user:P7EaMf2nZAc542Dy@cluster0.hnavmcc.mongodb.net/");
+        await mongoose.connect("mongodb+srv://prabhakardevagupthapu24_db_user:P7EaMf2nZAc542Dy@cluster0.hnavmcc.mongodb.net/Contactmanager?appName=Cluster0");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
